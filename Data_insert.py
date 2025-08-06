474c1a2d16652e6b3f4e38d5af3e6e3a3ed5b3b4
Khushi Yadav <kky957242@gmail.com>
2025-08-06 16:42:58 +0530

    Create Data_insert.py

---
+++ Data_insert.py
@@ -0,0 +1,13 @@
+import pandas as pd
+#Read data from CSV file into a DataFrames
+df = pd.read_csv("data.csv")
+
+#data write
+print("Full Data: ")
+print(df)
+#only show the city column
+print("\n Cities:")
+print(df['city'])
+#Filter : whoom Age >20
+print("\n Age>20:")
+print(df[df['Age']>20])
