3b1e39a0033f64e421163c7ea78a513440be7b48
Khushi Yadav <kky957242@gmail.com>
2026-01-09 21:13:40 +0530

    Create PandasTheory.py

---
+++ PandasTheory.py
@@ -0,0 +1,9 @@
+Pandas for Data Analyst – Complete Guide
+1️ What is Pandas?
+Pandas is a Python library used for:
+Data cleaning
+Data analysis
+Data manipulation
+Handling large datasets (CSV, Excel, SQL, JSON)
+Used by Data Analysts, Data Scientists, ML Engineers
+---------------------------------------------------------------------------------
